cdfad6f392e453c009c668fd0c39745b31afbaef
Pietro Caselani <pc1992@gmail.com>
2012-07-28 09:48:24 -0300

    Remove unused imports

--- src/com/pc/ScrollableTabView/ScrollableTabView.java
+++ src/com/pc/ScrollableTabView/ScrollableTabView.java
@@ -8,7 +8,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
